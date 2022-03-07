d18456a98c51d336c87a196b40fcc7af356784d9
LuongTran Thang <16h1090013@sv.ut.edu.vn>
2022-03-07 19:28:29 +0700

    find progression in console!

--- src/App.js
+++ src/App.js
@@ -3,25 +3,106 @@ import RunningText from "./components/header/RunningText.jsx";
 import "./components/header/RunningText.scss";
 import Vinyl from "./components/main/Vinyl";
 import "./components/main/Vinyl.scss";
-import Chord from "./components/sections/Chord";
-import "./components/sections/Chord.scss";
+import Chordify from "./components/sections/Chordify";
+import "./components/sections/Chordify.scss";
 import React, { useState, useEffect } from "react";
 
 function App() {
   const [runningText, setRunningText] = useState("Choose a chord");
   const [chord, setChord] = useState("");
   const [progression, setProgression] = useState("");
+  var scale = [];
+  var chordProg = [];
+
+  // return an array of scale of a key.
+  const findScale = (chord) => {
+    const chords = ["A", "B", "C", "D", "E", "F", "G"];
+    //create new array of chordsx2
+    var newArray = chords;
+    newArray.push(...chords);
+    //find the first index of the key
+    var indexKey = newArray.indexOf(chord);
+    //new array of scale
+    for (var i = 0; i < 6; i++) {
+      var minorPosition = [1, 2, 5];
+      if (minorPosition.indexOf(i) >= 0) {
+        scale.push(newArray[indexKey + i].concat("m"));
+      } else {
+        scale.push(newArray[indexKey + i]);
+      }
+    }
+    console.log(scale);
+  };
+
+  const convertToStr = (progression, chord) => {
+    console.log(chord);
+    console.log(progression);
+
+    var prog = [];
+    progression = progression.toUpperCase().split("-");
+    for (let i = 0; i < progression.length; i++) {
+      let string = "";
+      switch (progression[i]) {
+        case "I":
+          string = "1";
+          break;
+        case "II":
+          string = "2";
+          break;
+        case "III":
+          string = "3";
+          break;
+        case "IV":
+          string = "4";
+          break;
+        case "V":
+          string = "5";
+          break;
+        case "VI":
+          string = "6";
+          break;
+      }
+      prog.push(string); // progression in numbers
+    }
+    console.log(prog);
+    // return an array of scale of a key.
+    const chords = ["A", "B", "C", "D", "E", "F", "G"];
+    //create new array of chordsx2
+    var newArray = chords;
+    newArray.push(...chords);
+    //find the first index of the key
+    var indexKey = newArray.indexOf(chord);
+    //new array of scale
+    for (var i = 0; i < 6; i++) {
+      var minorPosition = [1, 2, 5];
+      if (minorPosition.indexOf(i) >= 0) {
+        scale.push(newArray[indexKey + i].concat("m"));
+      } else {
+        scale.push(newArray[indexKey + i]);
+      }
+    }
+    console.log(scale);
+    // convert progression into an array of chords
+    for (let j = 0; j < prog.length; j++) {
+      chordProg.push(scale[prog[j] - 1]);
+    }
+    console.log(chordProg);
+  };
+
   return (
     <div className="App">
       <div className="container">
         <RunningText runningText={runningText} />
         <Vinyl />
-        <Chord
+        <Chordify
           setRunningText={setRunningText}
           chord={chord}
           setChord={setChord}
           progression={progression}
           setProgression={setProgression}
+          findScale={findScale}
+          scale={scale}
+          convertToStr={convertToStr}
         />
       </div>
     </div>
--- src/components/sections/Chordify.jsx
+++ src/components/sections/Chordify.jsx
@@ -1,13 +1,24 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 const Chord = (props) => {
-  const { setRunningText, chord, setChord, progression, setProgression } =
-    props;
+  const {
+    setRunningText,
+    chord,
+    setChord,
+    progression,
+    setProgression,
+    findScale,
+
+    convertToStr,
+  } = props;
+  //Choose chord
   const next = () => {
     const str = document.getElementById("option1").value;
     setChord(str);
     setRunningText("Choose progression!!!");
+    findScale(str);
   };
+
   const previous = () => {
     setChord("");
     setProgression("");
@@ -17,7 +28,7 @@ const Chord = (props) => {
     const str = document.getElementById("option2").value;
     setProgression(str);
     setRunningText("Now playing...");
-
+    convertToStr(str, chord);
     const e = document.getElementById("vinyl");
     e.setAttribute("id", "vinyl-spin");
     const e2 = document.getElementById("arm");
@@ -26,7 +37,6 @@ const Chord = (props) => {
   const pause = () => {
     setProgression("");
     setRunningText("Choose progression!!!");
-
     const e = document.getElementById("vinyl-spin");
     e.setAttribute("id", "vinyl");
     const e2 = document.getElementById("arm-move");
@@ -47,13 +57,11 @@ const Chord = (props) => {
       {chord && !progression ? (
         <div>
           <select id="option2" size="4">
-            <option value="actual value 1">A</option>
-            <option value="actual value 2">B</option>
-            <option value="actual value 3">C</option>
-            <option value="actual value 4">D</option>
-            <option value="actual value 5">E</option>
-            <option value="actual value 6">F</option>
-            <option value="actual value 7">G</option>
+            <option value="I-V-vi-IV">I-V-vi-IV</option>
+            <option value="I-IV-V-IV">I-IV-V-IV</option>
+            <option value="I-vi-IV-V">I-vi-IV-V</option>
+            <option value="I-V-iv-iii">I-V-iv-iii</option>
+            <option value="IV-I-IV-V">IV-I-IV-V</option>
           </select>
           <div>
             <input
@@ -76,13 +84,11 @@ const Chord = (props) => {
       ) : chord && progression ? (
         <div>
           <select size="4" disabled>
-            <option value="actual value 1">A</option>
-            <option value="actual value 2">B</option>
-            <option value="actual value 3">C</option>
-            <option value="actual value 4">D</option>
-            <option value="actual value 5">E</option>
-            <option value="actual value 6">F</option>
-            <option value="actual value 7">G</option>
+            <option value="I-V-vi-IV">I-V-vi-IV</option>
+            <option value="I-IV-V-IV">I-IV-V-IV</option>
+            <option value="I-vi-IV-V">I-vi-IV-V</option>
+            <option value="I-V-iv-iii">I-V-iv-iii</option>
+            <option value="IV-I-IV-V">IV-I-IV-V</option>
           </select>
           <div>
             <input
@@ -105,13 +111,13 @@ const Chord = (props) => {
       ) : (
         <div>
           <select id="option1" size="4">
-            <option value="actual value 1">A</option>
-            <option value="actual value 2">B</option>
-            <option value="actual value 3">C</option>
-            <option value="actual value 4">D</option>
-            <option value="actual value 5">E</option>
-            <option value="actual value 6">F</option>
-            <option value="actual value 7">G</option>
+            <option value="A">A</option>
+            <option value="B">B</option>
+            <option value="C">C</option>
+            <option value="D">D</option>
+            <option value="E">E</option>
+            <option value="F">F</option>
+            <option value="G">G</option>
           </select>
           <div>
             <input type="button" value="" disabled />
